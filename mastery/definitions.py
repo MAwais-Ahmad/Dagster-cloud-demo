d2a28b706ab9ed3cdf4214df881ebfb1c224ec34
MAwais-Ahmad <awais.ahmad.de@gmail.com>
2025-12-16 20:13:02 +0500

    defs update

--- mastery/definitions.py
+++ mastery/definitions.py
@@ -2,11 +2,8 @@ from dagster import Definitions, load_assets_from_modules
 
 from . import assets, assets2
 
-asset1 = load_assets_from_modules([assets])
-asset2 = load_assets_from_modules([assets2])
-
 defs = Definitions(
-    assets=[asset1, asset2],
+    assets=[assets.raw_daily_sales, assets2.raw_daily_inventory],
     jobs=[assets.daily_sales_job, assets2.inventory_job],
     schedules=[assets.daily_schedule, assets2.inventory_schedule],
 )
